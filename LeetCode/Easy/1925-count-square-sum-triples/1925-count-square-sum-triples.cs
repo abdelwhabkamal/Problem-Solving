0c215911890a6407e6409a326e9802bbb68c8229
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-01-04 06:19:08 +0200

    12 ms (36.73%) 29.4 MB (56.74%) .cs

--- LeetCode/Easy/1925-count-square-sum-triples/1925-count-square-sum-triples.cs
+++ LeetCode/Easy/1925-count-square-sum-triples/1925-count-square-sum-triples.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int CountTriples(int n) {
         int ans =0;
-        for(int a=1;a<=n;a++){
+        for(int a=3;a<=n;a++){
             for(int b=a+1;b<=n;b++){
                 int ab=a*a+b*b;
                 for(int c=Math.Max(a,b);c<=n;c++){
