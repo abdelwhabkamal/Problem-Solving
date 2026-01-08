66600801f711a2c1852112b02ea6da34ff4215d7
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-01-08 10:04:29 +0200

    12 ms (46.19%) 79.6 MB (9.14%) .cs

--- LeetCode/Easy/0705-design-hashset/0705-design-hashset.cs
+++ LeetCode/Easy/0705-design-hashset/0705-design-hashset.cs
@@ -1,16 +1,25 @@
 public class MyHashSet {
-    private SortedList<int, bool> map = new();
-
+    int[] arr; 
+    public MyHashSet() {
+        arr =  new int[1000001]; 
+    }
+    
     public void Add(int key) {
-        if (!map.ContainsKey(key))
-            map[key] = true;
+        arr[key] = 1; 
     }
-
+    
     public void Remove(int key) {
-        map.Remove(key);
+        arr[key] = 0; 
+        
     }
-
-    public bool Contains(int key) {
-        return map.ContainsKey(key);
-    }
+    
+    public bool Contains(int key)=> arr[key] == 1 ? true: false;
 }
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * bool param_3 = obj.Contains(key);
+ */
