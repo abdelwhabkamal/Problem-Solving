dd694bf545a27877731256e7efd33f951487bc7b
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-01-08 09:46:28 +0200

    33 ms (19.8%) 71.5 MB (58.38%) .cs

---
+++ LeetCode/Easy/0705-design-hashset/0705-design-hashset.cs
@@ -0,0 +1,27 @@
+public class MyHashSet {
+    List<int> set = new();
+    public MyHashSet() {
+        set=new List<int>();
+    }
+    
+    public void Add(int key) {
+        if(!set.Contains(key)) set.Add(key);
+    }
+    
+    public void Remove(int key) {
+        set.Remove(key);
+    }
+    
+    public bool Contains(int key) {
+        if(set.Contains(key))return true;
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * bool param_3 = obj.Contains(key);
+ */
