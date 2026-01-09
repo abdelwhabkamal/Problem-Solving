3d55bbb3a50cd07f1eea5f6ff8b24ad9d5ac413d
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-01-09 15:01:55 +0200

    2546 ms (5.01%) 84.1 MB (6.9%) .cs

--- LeetCode/Medium/0912-sort-an-array/0912-sort-an-array.cs
+++ LeetCode/Medium/0912-sort-an-array/0912-sort-an-array.cs
@@ -1,12 +1,48 @@
-public class Solution {
-    public int[] SortArray(int[] nums) {
-        List<int> cp= new(nums);
-        List<int> result= new();
-        while(cp.Count != 0 ){
-            int min= cp.Min();
-            result.Add(min);
-            cp.Remove(min);
+public class Solution
+{
+    public int[] SortArray(int[] nums)
+    {
+        QuickSort(nums, 0, nums.Length - 1);
+        return nums;
+    }
+
+    private void QuickSort(int[] nums, int left, int right)
+    {
+        if (left >= right) return;
+
+        int pivotIndex = Partition(nums, left, right);
+        QuickSort(nums, left, pivotIndex - 1);
+        QuickSort(nums, pivotIndex + 1, right);
+    }
+
+    private static readonly Random rand = new Random();
+
+private int Partition(int[] nums, int left, int right)
+{
+    int pivotIndex = rand.Next(left, right + 1);
+    Swap(nums, pivotIndex, right);
+
+    int pivot = nums[right];
+    int i = left;
+
+    for (int j = left; j < right; j++)
+    {
+        if (nums[j] <= pivot)
+        {
+            Swap(nums, i, j);
+            i++;
         }
-        return result.ToArray();
+    }
+
+    Swap(nums, i, right);
+    return i;
+}
+
+
+    private void Swap(int[] nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
