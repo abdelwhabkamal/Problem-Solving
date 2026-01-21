776a45c79fb0d7d1902208f7a05549b71b4e5e91
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-01-21 16:03:30 +0200

    2 ms (37.5%) 51.3 MB (37.5%) .cs

--- LeetCode/Medium/3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cs
+++ LeetCode/Medium/3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cs
@@ -1,21 +1,23 @@
 public class Solution {
     public int[] MinBitwiseArray(IList<int> nums) {
-        var result = new int[nums.Count];
-        for(int i = 0; i < nums.Count; i++)
+    int[] res = new int[nums.Count];
+    for(int i = 0; i < nums.Count; i++)
+    {
+        if (nums[i] == 2)
         {
-            var mPow = (int)Math.Floor(Math.Log2(nums[i])); //get the position of the highest bit 0 indexed, 7 --> 2, 11 -> 3 etc
-            for(int j = mPow; j >= 0; j--) //start unsetting the highest bit position until the lowest.
-            {
-                var v = nums[i] - (int)Math.Pow(2, j); //calculate the value of the current hightest bit 7 --> 4, 11 -> 8, 17 -> 16
-                if(((v + 1 ) | v) == nums[i]) //check if its valid
-                {
-                    result[i] = v;
-                    break;
-                }
-            }
-            if(result[i] == 0)
-                result[i] =  ((nums[i] - 1) | nums[i]) == nums[i] ? nums[i] - 1 : -1; //if everything fails check itself and - 1, if this fails its edge case -1
+            res[i] = -1;
+            continue;
         }
-        return result;
+
+        int temp = nums[i];
+        int pow = -1;
+        while(temp > 0 && (temp & 1) == 1)
+        {
+            temp >>= 1;
+            pow++;
+        }
+        res[i] = nums[i] - (int)Math.Pow(2, pow);
+    }
+    return res;
     }
 }
