77562d011140b50c171212f17f46b7706cf9a0b2
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-01-24 17:02:43 +0200

    576 ms (72.73%) 65.9 MB (45.45%) .cs

--- LeetCode/Medium/1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cs
+++ LeetCode/Medium/1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cs
@@ -2,10 +2,10 @@ public class Solution {
     public int MinPairSum(int[] nums) {
         Array.Sort(nums);
         int l= nums.Length;
-        List<int> pairs= new();
+        int max_p=0;
         for(int i=0;i<l/2;i++){
-            pairs.Add(nums[i]+nums[l-1-i]);
+            max_p=Math.Max(nums[i]+nums[l-1-i],max_p);
         }
-        return pairs.Max();
+        return max_p;
     }
 }
