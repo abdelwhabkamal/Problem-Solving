53f684bdb6452d609e97f5baa1a33d3ec7db1fb4
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-01-25 03:07:59 +0200

    4 ms (100%) 45.5 MB (9.46%) .cs

--- LeetCode/Easy/1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cs
+++ LeetCode/Easy/1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cs
@@ -1,11 +1,13 @@
 public class Solution {
     public int MinimumDifference(int[] nums, int k) {
-        int n = nums.Length;
-        Array.Sort(nums);
+        if (nums.Length == 1) return 0;
+        Array.Sort(nums, (a, b) => b-a);
         int ans = int.MaxValue;
-        for (int i = 0; i + k - 1 < n; ++i) {
-            ans = Math.Min(ans, nums[i + k - 1] - nums[i]);
+        for(int i =0; i <= nums.Length-k; i++)
+        {
+            ans = Math.Min(ans, nums[i] - nums[i+k-1]);
         }
-        return ans;
+        
+        return ans;   
     }
 }
