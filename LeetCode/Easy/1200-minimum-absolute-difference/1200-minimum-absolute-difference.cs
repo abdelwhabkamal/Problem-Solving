cdbbdd0cc2fcccca235a11d00548eb1b101065ee
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-01-26 15:31:46 +0200

    50 ms (63.97%) 63.9 MB (82.35%) .cs

--- LeetCode/Easy/1200-minimum-absolute-difference/1200-minimum-absolute-difference.cs
+++ LeetCode/Easy/1200-minimum-absolute-difference/1200-minimum-absolute-difference.cs
@@ -3,12 +3,12 @@ public class Solution {
         int min=int.MaxValue;
         IList<IList<int>> ans= new List<IList<int>>();
         Array.Sort(arr);
-        for(int i=0;i<arr.Length-1;i++){
-            if(Math.Abs(arr[i]-arr[i+1])<min) min= Math.Abs(arr[i]-arr[i+1]);
+        for(int i=1;i<arr.Length;i++){
+            if(arr[i]-arr[i-1]<min) min= arr[i]-arr[i-1];
         }
-        for(int i=0;i<arr.Length-1;i++){
-            if(Math.Abs(arr[i]-arr[i+1])==min) 
-                ans.Add([arr[i],arr[i+1]]);
+        for(int i=1;i<arr.Length;i++){
+            if(arr[i]-arr[i-1]==min) 
+                ans.Add([arr[i-1],arr[i]]);
         }
         return ans;
     }
