a307e993123d6ffbf993d29d7abb5ab73598a348
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-01-28 13:38:06 +0200

    26 ms (5.06%) 44.8 MB (18.46%) .cs

--- LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
@@ -1,35 +1,31 @@
 public class Solution {
-    private int FastParse(string s) {
-        bool isNegetive = s[0] == '-';
-        int i = isNegetive ? 1 : 0;
-        int result = 0;
-        for (; i < s.Length; i++)
-            result = result * 10 + (s[i] - '0');
-        return isNegetive ? -result : result;
-    }
     public int EvalRPN(string[] tokens) {
+        Stack<int> stack= new();
         int val1=0;
         int val2=0;
-        Stack<int> stack = new Stack<int>();
-        foreach(string s in tokens){
-            if(s!="*" && s!="/" && s!="+" && s!="-"){
-                stack.Push(FastParse(s));
+        foreach(string t in tokens){
+            if(t != "+" && t != "-" && t != "*" && t != "/"){
+                stack.Push(int.Parse(t));
             }else{
-                val1=stack.Pop();         
-                val2=stack.Pop();
-                switch(s){
-                    case "+":
-                        stack.Push(val2+val1);
-                        break;
-                    case "-":
-                        stack.Push(val2-val1);
-                        break;
-                    case "*":
-                        stack.Push(val2*val1);
-                        break;
-                    case "/": 
-                        stack.Push(val2/val1);
-                        break;
+                if( t == "+"){
+                    val1=stack.Pop();
+                    val2=stack.Pop();
+                    stack.Push(val1+val2);
+                }
+                else if( t == "-"){
+                    val1=stack.Pop();
+                    val2=stack.Pop();
+                    stack.Push(val2-val1);
+                }
+                else if( t == "*"){
+                    val1=stack.Pop();
+                    val2=stack.Pop();
+                    stack.Push(val1*val2);
+                }
+                else{
+                    val1=stack.Pop();
+                    val2=stack.Pop();
+                    stack.Push(val2/val1);
                 }
             }
         }
