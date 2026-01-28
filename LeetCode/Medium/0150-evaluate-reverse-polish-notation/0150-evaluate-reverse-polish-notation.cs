bae8d6cbf4ac89db0b42c4c6931ee395e3b4852b
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-01-28 13:44:46 +0200

    3 ms (99.4%) 43.8 MB (96.82%) .cs

--- LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
@@ -1,11 +1,29 @@
 public class Solution {
+    private int StrToInt(string token)
+    {
+        int value = 0;
+        int multiplier = 1;
+        for (int i = 0; i < token.Length; i++)
+        {
+            if (token[i] == '+' || token[i] == '-')
+            {
+                multiplier = token[i] == '-' ? -1 : 1;
+            }
+            else
+            {
+                value = value*10 + (token[i] - '0');
+            }
+        }
+
+        return value * multiplier;
+    }
     public int EvalRPN(string[] tokens) {
         Stack<int> stack= new();
         int val1=0;
         int val2=0;
         foreach(string t in tokens){
             if(t != "+" && t != "-" && t != "*" && t != "/"){
-                stack.Push(int.Parse(t));
+                stack.Push(StrToInt(t));
             }else{
                 val1=stack.Pop();
                 val2=stack.Pop();
