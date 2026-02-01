f331351ef8d69c5ffa2b81030652072a92bc79f2
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-02-01 07:30:50 +0200

    3 ms (41.67%) 44.7 MB (66.67%) .cs

--- LeetCode/Easy/3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cs
+++ LeetCode/Easy/3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cs
@@ -2,11 +2,9 @@ public class Solution {
     public int MinimumCost(int[] nums) {
         int t=nums[0];
         Array.Sort(nums);
-        int f=nums[0];
-        int s=nums[1];
-        if(f != t && s != t)
-            return f+s+t;
+        if(nums[0] != t && nums[1] != t)
+            return nums[0]+nums[1]+t;
         else
-            return f+s+nums[2];
+            return nums[0]+nums[1]+nums[2];
     }
 }
