832e33f9a017a1add5ce90c12bf6affa733cb23e
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-02-06 10:05:24 +0200

    113 ms (20%) 76.2 MB (20%) .cs

--- LeetCode/Medium/3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cs
+++ LeetCode/Medium/3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cs
@@ -1,62 +1,22 @@
 public class Solution {
-    public int BinarySearch(int minInd,int[] nums,int k)
-    {
-        int n=nums.Length;
-        int lo=minInd+1;
-        int hi=n-1;
-        int  ans=-1;
-
-        while(lo<=hi)
-        {
-            int mid=(lo+hi)/2;
-
-            long min=nums[minInd];
-            long max=nums[mid];
-            long prod=min*k;
-            if(max<=prod)
-            {
-                ans=mid;
-                lo=mid+1;
-            }
-            else
-            {
-                hi=mid-1;
-            }
-            
+    public int MinRemoval(int[] nums, int k) {
+        if(nums.Length <= 1){
+            return 0;
         }
-
-        return ans;
-    }
-     
-    public int MinRemoval(int[] nums, int k) 
-    {
-        int n=nums.Length;
 
         Array.Sort(nums);
 
-        long min=nums[0];
-        long max=nums[n-1];
-        long prod=min*k;
-      
-        if(max<=prod)
-        {
-            return 0;
+        int i = 0;
+        var count = 0;
+        for(int j = 0; j < nums.Length; j++){
+           while(nums[j] > (long)k * (long)nums[i]){
+                i++;
+           } 
+
+            count = Math.Max(count, j - i + 1);
         }
         
-        int ans=int.MaxValue;
-        for(int i=0;i<n;i++)
-        {
-            int maxInd=BinarySearch(i,nums,k);
 
-            if(maxInd==-1)
-            {
-                continue;
-            }
-            
-            int removals=(n-maxInd-1)+i;
-            ans=Math.Min(ans,removals);
-        }
- 
-        return ans==int.MaxValue?n-1:ans;
+        return nums.Length - count;
     }
 }
