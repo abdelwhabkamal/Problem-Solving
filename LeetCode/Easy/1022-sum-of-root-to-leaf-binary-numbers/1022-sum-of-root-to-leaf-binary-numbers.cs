e4568ba884f84294808378f4d80fe99bc1cb205f
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-02-24 19:30:49 +0200

    0 ms (100%) 42.6 MB (29.63%) .cs

---
+++ LeetCode/Easy/1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * public int val;
+ * public TreeNode left;
+ * public TreeNode right;
+ * public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+public class Solution
+{
+    public int SumRootToLeaf(TreeNode root, int currentSum = 0)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+        
+        if (root.left == null && root.right == null)
+        {
+            currentSum = (currentSum << 1) | root.val;
+            return currentSum;
+        }
+        
+        currentSum = (currentSum << 1) | root.val;
+        return SumRootToLeaf(root.left, currentSum) + SumRootToLeaf(root.right, currentSum);
+    }
+}
