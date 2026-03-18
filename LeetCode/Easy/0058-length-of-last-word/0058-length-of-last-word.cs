1c534d39dfb1e7ddf186d8c7c65367974ed5f233
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-03-18 19:06:05 +0200

    0 ms (100%) 39.3 MB (99.46%) .cs

---
+++ LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        int ans = 0;
+        int i = s.Length - 1;
+
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s[i] != ' ') {
+            ans++;
+            i--;
+        }
+
+        return ans;
+    }
+}
