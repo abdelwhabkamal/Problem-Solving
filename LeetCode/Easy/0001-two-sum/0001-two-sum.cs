4cceba56973d94f98f130867830e7b8214a9b6d7
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-03-24 09:09:40 +0200

    4 ms (63.89%) 50 MB (18.31%) .cs

---
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int,int> indx=new();
+        for(int i=0;i<nums.Length;i++){
+            if(indx.ContainsKey(target-nums[i])){
+                return [indx[target-nums[i]],i];
+            }
+            indx[nums[i]]=i;
+        }
+        return [0,0];
+    }
+}
