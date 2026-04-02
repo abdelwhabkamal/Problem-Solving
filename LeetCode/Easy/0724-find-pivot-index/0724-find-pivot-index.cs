4d4f4f948572893be4314249bc004bbd72456b5d
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-02 14:46:27 +0200

    0 ms (100%) 49.2 MB (72.07%) .cs

--- LeetCode/Easy/0724-find-pivot-index/0724-find-pivot-index.cs
+++ LeetCode/Easy/0724-find-pivot-index/0724-find-pivot-index.cs
@@ -1,15 +1,18 @@
-public class Solution {
-    public int PivotIndex(int[] nums) 
+public class Solution
+{
+    public int PivotIndex(int[] nums)
     {
-        int total=0;
-        for(int i=0;i<nums.Length;i++){
-            total+=nums[i];
-        }
-        int left=0;
-        for(int i=0;i<nums.Length;i++){
-            if(left==total-nums[i]-left)
-                return i;
-            left+=nums[i];
+        int leftSum = 0, pivotIndex;
+        int rightSum = 0;
+        for (int i = nums.Length - 1; i >= 0; i--)
+            rightSum += nums[i];
+        for (pivotIndex = 0; pivotIndex < nums.Length; pivotIndex++)
+        {
+            if (pivotIndex - 1 != -1)
+                leftSum += nums[pivotIndex - 1];
+            rightSum -= nums[pivotIndex];
+            if (leftSum == rightSum)
+                return pivotIndex;
         }
         return -1;
     }
