1df3d4da3f70bc5010219abb6bf10e166b69a964
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-06 16:04:11 +0200

    6 ms (71.71%) 44.9 MB (22.54%) .cs

--- LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
@@ -1,39 +1,19 @@
 public class Solution {
-    private int StrToInt(string token)
-    {
-        int value = 0;
-        int multiplier = 1;
-        for (int i = 0; i < token.Length; i++)
-        {
-            if (token[i] == '+' || token[i] == '-')
-            {
-                multiplier = token[i] == '-' ? -1 : 1;
-            }
-            else
-            {
-                value = value*10 + (token[i] - '0');
-            }
-        }
-
-        return value * multiplier;
-    }
     public int EvalRPN(string[] tokens) {
         Stack<int> stack= new();
         int val1=0;
         int val2=0;
-        foreach(string t in tokens){
-            if(t != "+" && t != "-" && t != "*" && t != "/"){
-                stack.Push(StrToInt(t));
-            }else{
-                val1=stack.Pop();
+        for(int i=0; i<tokens.Length; i++){
+            if(tokens[i] != "+" && tokens[i] != "*" && tokens[i] != "-" && tokens[i] != "/" ){
+                stack.Push(int.Parse(tokens[i]));
+            }
+            else{
                 val2=stack.Pop();
-                switch(t){
-                    case "+": stack.Push(val1+val2); break;
-                    case "-": stack.Push(val2-val1); break;
-                    case "*": stack.Push(val1*val2); break;
-                    case "/": stack.Push(val2/val1); break;
-                }
-                    
+                val1=stack.Pop();
+                if(tokens[i]=="*")  stack.Push(val1*val2);
+                else if(tokens[i]=="/")  stack.Push(val1/val2);
+                else if(tokens[i]=="-")  stack.Push(val1-val2);
+                else stack.Push(val1+val2);
             }
         }
         return stack.Pop();
