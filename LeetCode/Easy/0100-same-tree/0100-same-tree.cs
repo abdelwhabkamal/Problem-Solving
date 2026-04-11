a2ddbbdd6c51b6e79b6ffc3ad485d41bf776c13d
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-11 13:01:28 +0200

    0 ms (100%) 42.9 MB (93.8%) .cs

--- LeetCode/Easy/0100-same-tree/0100-same-tree.cs
+++ LeetCode/Easy/0100-same-tree/0100-same-tree.cs
@@ -13,8 +13,9 @@
  */
 public class Solution {
     public bool IsSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q ==null)return true;
-        if(p==null || q ==null || p.val != q.val)return false;
-        return IsSameTree(p.right,q.right) && IsSameTree(p.left,q.left);
+        if(p == null && q == null ) return true;
+        if(p == null || q == null ) return false;
+        if(p.val == q.val) return IsSameTree(p.right , q.right) && IsSameTree(p.left , q.left);
+        return false;
     }
 }
