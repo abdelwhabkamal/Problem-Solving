aca4ba198bafb39e751ac16a2de48ab846bf5589
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-11 12:55:37 +0200

    0 ms (100%) 41.9 MB (15.44%) .cs

--- LeetCode/Easy/0226-invert-binary-tree/0226-invert-binary-tree.cs
+++ LeetCode/Easy/0226-invert-binary-tree/0226-invert-binary-tree.cs
@@ -14,11 +14,9 @@
 public class Solution {
     public TreeNode InvertTree(TreeNode root) {
         if(root==null) return null;
-        TreeNode tmp=root.left;
-        root.left=root.right;
-        root.right=tmp;
-        InvertTree(root.left);
-        InvertTree(root.right);
-        return root;
+        TreeNode inverted= new TreeNode(root.val);
+        inverted.left = InvertTree(root.right);
+        inverted.right = InvertTree(root.left);
+        return inverted;
     }
 }
