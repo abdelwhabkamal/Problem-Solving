c8d16e8fc78af1fe9a8645895ed8ac662cc55a72
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-13 12:13:36 +0200

    0 ms (100%) 47.4 MB (50.95%) .cs

---
+++ LeetCode/Medium/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cs
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<int> RightSideView(TreeNode root) {
+        IList<int> ans = new List<int>();
+        if(root == null) return ans;  
+        Queue<TreeNode> queue = new();
+        queue.Enqueue(root);
+        while(queue.Count>0){
+            int level = queue.Count;
+            TreeNode curr = new();
+            int val=0;
+            for(int i = 0 ; i < level; i++){
+                curr = queue.Dequeue();
+                val = curr.val;
+                if(curr.left != null ) queue.Enqueue(curr.left);
+                if(curr.right != null ) queue.Enqueue(curr.right);
+            }
+            ans.Add(val);
+             
+        }
+        return ans;
+    }
+}
