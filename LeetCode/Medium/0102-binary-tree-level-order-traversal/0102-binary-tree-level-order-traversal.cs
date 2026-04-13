4d6d2379abe27754c76424c0e3c134afaee8ba6a
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-13 11:48:02 +0200

    N/A (0%) N/A (0%) .cs

---
+++ LeetCode/Medium/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cs
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> LevelOrder(TreeNode root) {
+        IList<IList<int>> ans = new List<IList<int>>();
+        if(root==null) return ans;
+        Queue<TreeNode> queue= new();
+        queue.Enqueue(root);
+        while(queue.Count>0){
+            List<int> list = new();
+            TreeNode curr = new();
+            int level = queue.Count();
+            for(int i = 0; i < level; i++){
+                curr = queue.Dequeue();
+                list.Add(curr.val);
+                if(curr.left != null) queue.Enqueue(curr.left);
+                if(curr.right != null) queue.Enqueue(curr.right);
+            }
+            ans.Add(list);
+            
+        }
+        return ans;
+    }
+}
