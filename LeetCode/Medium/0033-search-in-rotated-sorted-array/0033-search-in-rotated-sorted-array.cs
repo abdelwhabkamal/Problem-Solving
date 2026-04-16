de18fdac6315e70721aa606624b1f112b49a493a
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-16 08:07:01 +0200

    0 ms (100%) 42.6 MB (24.91%) .cs

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cs
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cs
@@ -1,28 +1,21 @@
 public class Solution {
     public int Search(int[] nums, int target) {
-        int l = 0, r = nums.Length - 1;
-        
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            
-            if (nums[mid] == target) return mid;
-            
-            if (nums[l] <= nums[mid]) {
-                if (nums[l] <= target && target < nums[mid]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
-            }
-            else {
-                if (nums[mid] < target && target <= nums[r]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
-            }
-        }
-        
+        int n = nums.Length;
+        int l = 0;
+        int r = n-1;
+        int mid = 0;
+        while(l<r){
+            mid = l + (r - l) / 2;
+            if(nums[mid] < nums[r]) r = mid;
+            else l = mid + 1;
+        } 
+        r = l + n - 1;
+        while(r>=l){
+            mid = l + (r - l) / 2;
+            if(nums[(mid+n) % n] == target) return (mid+n) % n;
+            if( nums[(mid+n) % n] > target) r = mid -1;
+            else l = mid + 1;
+        } 
         return -1;
     }
 }
