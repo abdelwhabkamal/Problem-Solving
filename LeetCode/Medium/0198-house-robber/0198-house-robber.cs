6fd39b5d3e6576b0fde0cc602691c6e09164cd45
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-18 13:26:22 +0200

    N/A (0%) N/A (0%) .cs

--- LeetCode/Medium/0198-house-robber/0198-house-robber.cs
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.cs
@@ -4,11 +4,11 @@ public class Solution {
         if(n<=2) return nums.Max();
         int val1 = nums[0];
         int val2 = Math.Max(nums[1],val1);
-        int ans = 0;
-        for(int i=2 ; i<n ; i++){
-            ans = Math.Max(val2 , nums[i] + val1);
+        int ans = Math.Max(val2 , nums[2] + val1);
+        for(int i=3 ; i<n ; i++){
             val1 = val2;
             val2 = ans; 
+            ans = Math.Max(val2 , nums[i] + val1);
         }
         return ans;
     }
