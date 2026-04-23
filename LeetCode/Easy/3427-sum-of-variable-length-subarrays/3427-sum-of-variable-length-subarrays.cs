0764b9695586b339cda773a26690308f23198ae8
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-23 15:33:18 +0200

    N/A (0%) N/A (0%) .cs

--- LeetCode/Easy/3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.cs
+++ LeetCode/Easy/3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.cs
@@ -1,11 +1,14 @@
 public class Solution {
     public int SubarraySum(int[] nums) {
         int res = 0;
+        int n = nums.Length;
+        int[] prefix = new int[n + 1];
+        for(int i = 0; i < n; i++){
+            prefix[i+1] = prefix[i] + nums[i];
+        }
         for(int i = 0; i < nums.Length; i++){
             int start = Math.Max(0 , i - nums[i]);
-            for(int j = start; j <= i; j++){
-                res += nums[j];
-            }
+            res += prefix[i+1] - prefix[start];
         }
         return res;
     }
