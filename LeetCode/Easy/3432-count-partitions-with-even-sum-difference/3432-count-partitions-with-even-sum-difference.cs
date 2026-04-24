b2916f55c66b2d5639f1222d6878ea73cc034dc0
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-24 10:30:24 +0300

    N/A (0%) N/A (0%) .cs

--- LeetCode/Easy/3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cs
+++ LeetCode/Easy/3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cs
@@ -1,12 +1,15 @@
 public class Solution {
     public int CountPartitions(int[] nums) {
-        int sum = nums.Sum();
+        int sum = 0;
+        foreach(int num in nums){
+            sum += num;
+        }
         int prefixSum = 0;
         int res = 0;
         for(int i = 0; i < nums.Length-1; i++){
             prefixSum += nums[i];
             sum -= nums[i];
-            if(Math.Abs(prefixSum-sum) % 2 == 0) res++;
+            if((prefixSum-sum) % 2 == 0) res++;
         }
         return res;
     }
