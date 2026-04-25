eb5f181c3845e5c13537a26ff148dddba744f7e1
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-25 14:51:55 +0300

    N/A (0%) N/A (0%) .cs

---
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -0,0 +1,16 @@
+public class Solution 
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        int i1 = m-1;
+        int i2 = n-1;
+        int p = m + n - 1;
+        while( i2 >= 0 ){
+            if(i1 >= 0 && nums1[i1] > nums2[i2]){
+                nums1[p--] = nums1[i1--];
+            }
+            else
+                nums1[p--] = nums2[i2--];
+        }
+    }
+}
