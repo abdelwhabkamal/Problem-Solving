bf96d55303a84ac9c1e0f522648423868421c54c
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-04-29 15:53:49 +0300

    137 ms (69%) 53.3 MB (25.34%) .cs

--- LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cs
+++ LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cs
@@ -2,13 +2,13 @@ public class Solution {
     public int RemoveDuplicates(int[] nums) {
         int n = nums.Length;
         if(n <= 2) return n;
-        int res  = 2;
-        for(int i = 2; i < n ; i++){
-            if(nums[i] != nums[res-2]){
-                nums[res] = nums[i];
-                res++;
+        int k = 2;
+        for(int i = 2; i < n; i++){
+            if(nums[i] != nums[k-2]){
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return res;
+        return k;
     }
 }
