acfe71d04f1c5fb119c5a6955ffb62b09cf7cd2f
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-05-02 14:27:16 +0300

    1 ms (100%) 63.6 MB (29.93%) .cs

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -5,13 +5,12 @@ public class Solution {
         int[] suf=new int[n];
         pref[0]=1;
         suf[n-1]=1;
-        for(int i=1;i<n;i++){
-            pref[i]=nums[i-1]*pref[i-1];
-            suf[n-1-i]=nums[n-i]*suf[n-i];
+        for( int i = 0; i < n-1; i++){
+            pref[i + 1] = nums[i] * pref[i];
+            suf[n-2-i] = nums[n-i-1] * suf[n-i-1]; 
         }
-        for(int i=0;i<n;i++)
-        {   
-            nums[i]=pref[i]*suf[i];
+        for(int i = 0; i < n; i++){
+            nums[i] = pref[i] * suf[i];
         }
         return nums;        
     }
