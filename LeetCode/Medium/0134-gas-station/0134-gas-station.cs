1d437a119582e17cdda6d67b98cd803318f98c73
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-05-03 17:45:20 +0300

    N/A (0%) N/A (0%) .cs

--- LeetCode/Medium/0134-gas-station/0134-gas-station.cs
+++ LeetCode/Medium/0134-gas-station/0134-gas-station.cs
@@ -1,6 +1,5 @@
 public class Solution {
     public int CanCompleteCircuit(int[] gas, int[] cost) {
-        if(gas.Sum() < cost.Sum()) return -1;
         int n = gas.Length;
         int tank = 0;
         int res = 0;
@@ -11,6 +10,6 @@ public class Solution {
                 tank = 0;
             }
         }
-        return res;
+        return tank > 0 ? res : -1;
     }
 }
