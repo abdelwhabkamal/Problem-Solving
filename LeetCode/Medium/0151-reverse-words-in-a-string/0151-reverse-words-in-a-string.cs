b408f07a46aaedb48f609bc7591cdd2c5e4939b9
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-05-04 18:28:55 +0300

    N/A (0%) N/A (0%) .cs

--- LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
+++ LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -1,41 +1,13 @@
-public class Solution
-{
-    public string ReverseWords(string s)
-    {
-        StringBuilder result = new();
-
-        for (int i = s.Length - 1; i >= 0; i--)
+public class Solution {
+    public string ReverseWords(string s) {
+        string[] str = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        StringBuilder result = new StringBuilder();
+        for(int i =str.Length-1; i>0;i--)
         {
-            if (s[i] == ' ')
-                continue;
-
-            int j = i;
-            while (j > 0 && s[j - 1] != ' ')
-            {
-                j--;
-            }
-
-            result.Append(s[j..(i + 1)]);
-
-            bool lastSpace = false;
-            int k = j - 1;
-            while (k >= 0)
-            {
-                if (s[k] != ' ')
-                {
-                    lastSpace = true;
-                    break;
-                }
-
-                k--;
-            }
-
-            if (lastSpace)
-                result.Append(' ');
-
-            i = j;
+            result.Append(str[i]);
+            result.Append(' ');
         }
-
+        result.Append(str[0]);
         return result.ToString();
     }
 }
