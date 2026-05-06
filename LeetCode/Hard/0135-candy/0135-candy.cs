787457b65894be8a597c2e77020496a04b57d918
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-05-06 10:37:24 +0300

    N/A (0%) N/A (0%) .cs

--- LeetCode/Hard/0135-candy/0135-candy.cs
+++ LeetCode/Hard/0135-candy/0135-candy.cs
@@ -2,20 +2,22 @@ public class Solution {
     public int Candy(int[] ratings) {
         int n = ratings.Length;
         int[] candies = new int[n];
-        Array.Fill(candies , 1);
-        
+        candies[0] = 1;
+
         for(int i = 1; i < n; i++){
+            candies[i] = 1;
             if(ratings[i] > ratings[i-1]){
                 candies[i] = candies[i-1] + 1;
             }
         }
-
+        int sum = candies[n-1];
         for(int i = n-1; i > 0; i--){
             if(ratings[i-1] > ratings[i]){
                 candies[i-1] = Math.Max(candies[i] + 1 , candies[i-1]);
             }
+            sum += candies[i];
         }
-        
-        return candies.Sum();
+
+        return sum;
     }
 }
