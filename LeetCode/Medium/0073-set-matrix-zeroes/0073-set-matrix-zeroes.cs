81ebe82751ba5af22a4ea7444680c6081eba3885
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-05-11 04:58:30 +0300

    N/A (0%) N/A (0%) .cs

--- LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cs
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cs
@@ -1,22 +1,23 @@
 public class Solution {
     public void SetZeroes(int[][] matrix) {
-        List<int> zeros = new();
         int m = matrix[0].Length;
         int n = matrix.Length;
+        bool[] rows = new bool[n];
+        bool[] cols = new bool[m];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(matrix[i][j] == 0) zeros.Add((i * m) + j);
+                if(matrix[i][j] == 0){
+                    rows[i] = true;
+                    cols[j] = true;
+                }
             }
         }
-        for(int i = 0; i < zeros.Count; i++){
-            int row = zeros[i] / m;
-            int col = zeros[i] % m;
+        for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                matrix[row][j] = 0;
+                if(rows[i] || cols[j]){
+                    matrix[i][j] = 0;
+                }
             }  
-            for(int j = 0; j < n; j++){
-                matrix[j][col] = 0;
-            }
         }
     }
 }
