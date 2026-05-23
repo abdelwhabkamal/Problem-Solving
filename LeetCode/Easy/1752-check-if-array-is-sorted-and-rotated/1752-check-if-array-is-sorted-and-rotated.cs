b68e9d56eff729656556637782e39f12c6c7c31b
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-05-23 17:26:23 +0300

    0 ms (100%) 43.4 MB (26.25%) .cs

--- LeetCode/Easy/1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cs
+++ LeetCode/Easy/1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cs
@@ -1,20 +1,12 @@
 public class Solution {
     public bool Check(int[] nums) {
         int n = nums.Length;
-        int l = 0;
-        int r = n-1;
-        int min = 0;
-        if( nums[r] > nums[l]) min = nums[l];
-        while(l < n - 1 && nums[l] == nums[r]) l++;
-        while(l<r){
-            int mid = l + (r - l) / 2;
-            if(nums[mid] <= nums[r] ) r = mid ;
-            else l = mid + 1;
-        } 
-        min = nums[l];
-        if(nums.Min() != min) return false;
-        for(int i = l; i < l+n-1; i++){
-            if(nums[ (i + n) % n] > nums[ (i + 1 + n) % n]) return false;
+        bool flag = false;
+        for(int i = 0; i< n; i++){
+            if(nums[i] > nums[ ( i + 1) % n]){
+                if(flag) return false;
+                flag = true;
+            }
         }
         return true;
     }
