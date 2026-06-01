016fe2228be32368efa198a6e7a8a30b0a74f374
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-06-01 15:36:05 +0300

    3 ms (75%) 42.7 MB (12.5%) .cs

--- LeetCode/Easy/2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cs
+++ LeetCode/Easy/2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cs
@@ -1,10 +1,8 @@
 public class Solution {
     public int MinimumCost(int[] cost) {
-        int n = cost.Length;
-        if(n <= 2) return cost.Sum();
         cost.Sort();
         int res = 0;
-        for(int i = n-1; i >= 0; i-=3){
+        for(int i = cost.Length-1; i >= 0; i-=3){
             res += cost[i];
             if( i-1 >= 0) res += cost[i-1];
         }
