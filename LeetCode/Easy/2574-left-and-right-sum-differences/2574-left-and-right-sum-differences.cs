ab3672c4f258cee5596154c78ac8090df126a1ec
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-06-06 12:16:06 +0300

    3 ms (57.41%) 51 MB (41.67%) .cs

--- LeetCode/Easy/2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cs
+++ LeetCode/Easy/2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cs
@@ -1,14 +1,13 @@
 public class Solution {
     public int[] LeftRightDifference(int[] nums) {
-        int n = nums.Length;
-        int[] res = new int[n];
+        int[] res = new int[nums.Length];
         int left = 0;
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i < nums.Length; i++){
             res[i] = res[i-1] + nums[i-1];
             left += res[i];
         }
         int right = 0;
-        for(int i = n-2; i >= 0; i--){
+        for(int i = nums.Length-2; i >= 0; i--){
             right += nums[i + 1];
             res[i] = Math.Abs(res[i] - right);
         }      
