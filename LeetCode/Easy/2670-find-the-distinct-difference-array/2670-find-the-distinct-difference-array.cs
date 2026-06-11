e9678783785a7439d3fd2375f636c57a194604ba
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-06-11 21:24:01 +0300

    7 ms (73.33%) 60.9 MB (86.67%) .cs

---
+++ LeetCode/Easy/2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] DistinctDifferenceArray(int[] nums) {
+        int[] suff = new int[51];
+        for(int i = 0; i < nums.Length; i++){
+            suff[nums[i]]++;
+        }
+        int s = 0;
+        for(int i = 1; i <= 50; i++){
+            if(suff[i] > 0) s++;
+        }
+        int[] pref = new int[51];
+        int p = 0;
+        int[] res = new int[nums.Length];
+        for(int i = 0; i < nums.Length; i++){
+            if(pref[nums[i]] == 0){
+                p++;
+                pref[nums[i]]++;
+            }
+            else pref[nums[i]]++;
+            suff[nums[i]]--;
+            if(suff[nums[i]] == 0) s--;
+            res[i] = p - s;
+        }
+        return res;
+    }
+}
