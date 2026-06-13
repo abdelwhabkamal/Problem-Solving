b135de6b800aafc2e54951112f380180790a52e7
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-06-13 16:29:05 +0300

    3 ms (42.22%) 52.2 MB (8.89%) .cs

--- LeetCode/Easy/3838-weighted-word-mapping/3838-weighted-word-mapping.cs
+++ LeetCode/Easy/3838-weighted-word-mapping/3838-weighted-word-mapping.cs
@@ -1,14 +1,14 @@
 public class Solution {
     public string MapWordWeights(string[] words, int[] weights) {
-        StringBuilder res = new();
+        string res = "";
         for(int i = 0; i < words.Length; i++){
             int val = 0;
             foreach(char c in words[i]){
                 val += weights[c - 'a'];
             }
             val %= 26;
-            res.Append((char) (122 - val));
+            res += (char) (122 - val);
         }
-        return res.ToString(); 
+        return res; 
     }
 }
