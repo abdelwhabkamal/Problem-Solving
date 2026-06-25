af8913cf49dc85484309576742f45823d47cb18c
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-06-25 08:09:11 +0300

    46 ms (40%) 49.9 MB (60%) .cs

--- LeetCode/Medium/3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cs
+++ LeetCode/Medium/3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cs
@@ -1,41 +1,16 @@
 public class Solution {
     public int CountMajoritySubarrays(int[] nums, int target) {
         int n = nums.Length;
-        int[] balance = new int[n + 1];
-        
-        for (int i = 0; i < n; i++) {
-            balance[i + 1] = balance[i] + (nums[i] == target ? 2 : 0) - 1;
-        }
-
-        var sorted = balance.Distinct().OrderBy(x => x).ToList();
-        var indexMap = new Dictionary<int, int>();
-        for (int i = 0; i < sorted.Count; i++) indexMap[sorted[i]] = i + 1; 
-
-        int[] bit = new int[sorted.Count + 2];
-        int res = 0;
-
-        void Update(int idx) {
-            while (idx < bit.Length) {
-                bit[idx]++;
-                idx += idx & -idx;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = i; j < n; ++j) {
+                cnt += (nums[j] == target ? 1 : -1);
+                if (cnt > 0) {
+                    ++ans;
+                }
             }
         }
-
-        int Query(int idx) {
-            int sum = 0;
-            while (idx > 0) {
-                sum += bit[idx];
-                idx -= idx & -idx;
-            }
-            return sum;
-        }
-
-        foreach (var b in balance) {
-            int idx = indexMap[b];
-            res += Query(idx - 1);
-            Update(idx);
-        }
-
-        return res;
+        return ans;
     }
 }
