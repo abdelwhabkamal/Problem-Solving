4ba6f00e4471217c53c2314785cedbdcbf3ba0a9
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-07-01 12:50:05 +0300

    0 ms (100%) 64.8 MB (62.29%) .cs

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.cs
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.cs
@@ -2,9 +2,9 @@ public class Solution {
     public void ReverseString(char[] s) {
         int n = s.Length;
         for(int i = 0; i < n/2 ; i++){
-            s[i] += s[n - i - 1] ;
-            s[n - i - 1] = (char) ( s[i] - s[n - i - 1]);
-            s[i] = (char)(s[i] - s[n - i - 1]);
+            s[i] ^= s[n - i - 1];
+            s[n - i - 1] ^= s[i];
+            s[i] ^= s[n - i - 1];
         }
     }
 }
