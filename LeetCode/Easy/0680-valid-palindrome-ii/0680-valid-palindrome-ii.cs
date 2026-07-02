b7d9160430b192db27cf5fbef88c906bc1141232
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-07-02 13:21:27 +0300

    2 ms (100%) 52.4 MB (86.75%) .cs

--- LeetCode/Easy/0680-valid-palindrome-ii/0680-valid-palindrome-ii.cs
+++ LeetCode/Easy/0680-valid-palindrome-ii/0680-valid-palindrome-ii.cs
@@ -1,38 +1,22 @@
 public class Solution {
     public bool ValidPalindrome(string s) {
-        int l = 0;
-        int r = s.Length - 1;
-        bool flag = true;
-        bool res = true;
-        int new_l = 0;
-        int new_r = 0;
-        while(r >= l){
-            if(!flag && s[l] != s [r]){
-                res = false;
-                break;
-            }
-            if(flag &&  s[l] != s [r]){
-                new_l = l;
-                new_r = r-1;
-                l++;
-                if(s[l] != s[r]){
-                    res = false;
-                    break;
-                } 
-                flag = false;
+        int l = 0, r = s.Length - 1;
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return IsPalindromeRange(s, l + 1, r) || IsPalindromeRange(s, l, r - 1);
             }
             l++;
             r--;
         }
+        return true;
+    }
 
-        if (res) return res;
-        
-        while(new_r >= new_l){
-            if(s[new_r] != s[new_l]) return false;
-            new_r--;
-            new_l++;
+    private bool IsPalindromeRange(string s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
         }
-        
         return true;
     }
 }
