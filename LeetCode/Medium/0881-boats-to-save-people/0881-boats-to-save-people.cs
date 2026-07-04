a297d7431b8837abb901f0ceec0330aed46f029a
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-07-04 16:30:03 +0300

    70 ms (21.9%) 51 MB (62.77%) .cs

---
+++ LeetCode/Medium/0881-boats-to-save-people/0881-boats-to-save-people.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int NumRescueBoats(int[] people, int limit) {
+        Array.Sort(people);
+        int res = 0;
+        int l = 0 , r = people.Length-1;
+        while(l <= r){
+            if(people[l] + people[r] <= limit) l++;
+            res++;
+            r--;
+        }
+        return res;
+    }
+}
