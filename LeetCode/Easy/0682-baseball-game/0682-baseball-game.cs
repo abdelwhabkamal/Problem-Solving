1ef2872423dc16eb1400b99af4b28a00f87e622c
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-07-05 17:33:52 +0300

    3 ms (42.99%) 41.9 MB (43.89%) .cs

--- LeetCode/Easy/0682-baseball-game/0682-baseball-game.cs
+++ LeetCode/Easy/0682-baseball-game/0682-baseball-game.cs
@@ -1,25 +1,30 @@
 public class Solution {
     public int CalPoints(string[] operations) {
         Stack<int> stack = new();
-        
+
         foreach (string o in operations) {
-            if (int.TryParse(o, out int num)) {
-                stack.Push(num);
-            } 
-            else if (o == "C") {
-                stack.Pop();
-            } 
-            else if (o == "D") {
-                stack.Push(2 * stack.Peek());
-            } 
-            else if (o == "+") {
-                int top = stack.Pop();
-                int newTop = top + stack.Peek();
-                stack.Push(top);       
-                stack.Push(newTop);    
+            switch (o) {
+                case "C":
+                    stack.Pop();
+                    break;
+
+                case "D":
+                    stack.Push(2 * stack.Peek());
+                    break;
+
+                case "+":
+                    int top = stack.Pop();
+                    int newTop = top + stack.Peek();
+                    stack.Push(top);       
+                    stack.Push(newTop);    
+                    break;
+
+                default:
+                    stack.Push(int.Parse(o));
+                    break;
             }
         }
-        
+
         return stack.Sum();
     }
 }
