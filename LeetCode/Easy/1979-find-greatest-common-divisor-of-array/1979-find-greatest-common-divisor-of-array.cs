636f88c889410b9c4a0a219b8d8cd3deaa41d930
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-07-18 06:21:34 +0300

    0 ms (100%) 43.5 MB (65.52%) .cs

--- LeetCode/Easy/1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cs
+++ LeetCode/Easy/1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cs
@@ -1,7 +1,11 @@
 public class Solution {
     public int FindGCD(int[] nums) {
-        int max = nums.Max();
-        int min = nums.Min();
+        int min = 1001;
+        int max = 0;
+        foreach(int n in nums){
+            if(n > max) max = n;
+            if(n < min) min = n;
+        }
         int res = min;
         while(min > 0){
             if(max % res == 0 && min % res == 0) return res;
