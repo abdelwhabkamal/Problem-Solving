7a68c56ff9779e7457250d7c2b21e78fa6e090b4
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-07-22 05:52:29 +0300

    320 ms (99.71%) 0B (100%) .sql

--- LeetCode/Medium/0608-tree-node/0608-tree-node.sql
+++ LeetCode/Medium/0608-tree-node/0608-tree-node.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
 select id , case
         when p_id is null then 'Root'
-        when id not in (select distinct p_id from tree where p_id is not null) then 'Leaf'
-        else 'Inner'
+        when id in (select distinct p_id from tree where p_id is not null) then 'Inner'
+        else 'Leaf'
         end as type
 from tree
