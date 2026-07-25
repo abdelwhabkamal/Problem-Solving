8ec4d6f0613cd396ad52ba817ff2f789d24f804c
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-07-25 13:02:39 +0300

    0 ms (100%) 29.1 MB (56.25%) .cs

---
+++ LeetCode/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MaxProduct(int n) {
+        int max1 = 0; 
+        int max2 = 0; 
+        while(n > 0){
+            if(max1 <= n%10){
+                max2 = max1;
+                max1 = n%10;
+            }
+            else if(n%10 > max2) max2 = n%10;
+            n /= 10;
+        }
+        return max1 * max2;
+    }
+}
