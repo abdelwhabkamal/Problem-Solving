cd4b65ff24e4b240c2ce21f8147fb39284c1fb9d
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-01 18:15:19 +0300

    0 ms (100%) 40.8 MB (22.22%) .cs

--- LeetCode/Easy/3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.cs
+++ LeetCode/Easy/3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.cs
@@ -1,12 +1,6 @@
 public class Solution {
     public string WinningPlayer(int x, int y) {
-        int num = 0;
-        while(x > 0 && y > 3){
-            num++;
-            x--;
-            y -= 4;
-        }
-        if(num % 2 == 0 || num == 0) return "Bob";
+        if(Math.Min(x, y/4) % 2 == 0) return "Bob";
         return "Alice";
     }
 }
