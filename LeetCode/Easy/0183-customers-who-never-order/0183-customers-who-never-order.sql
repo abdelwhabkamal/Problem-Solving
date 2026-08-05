6214dc59b46b021b09f918fc8efdd70473368cf9
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-05 20:14:58 +0300

    896 ms (5.02%) 0B (100%) .sql

--- LeetCode/Easy/0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ LeetCode/Easy/0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,3 +1,3 @@
 /* Write your T-SQL query statement below */
-select name as Customers from customers left join orders on customers.id = orders.customerId
-where orders.customerId is null
+select c.name as Customers from customers as c left join orders as o on c.id = o.customerId
+where o.customerId is null
