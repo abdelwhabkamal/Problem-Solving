9397fa8185d3ca41b879ba5d12da4a3c4fa65dce
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-09 19:14:07 +0300

    0 ms (100%) 28.5 MB (98.21%) .cs

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.cs
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.cs
@@ -1,14 +1,13 @@
 public class Solution {
     public int ClimbStairs(int n) {
-        if(n <= 2) return n;
-        int val1 = 1;
-        int val2 = 2;
-        int ans = 0;
-        for(int i = 2; i < n; i++){
-            ans = val1 + val2;
-            val1 = val2;
-            val2 = ans;
+        if(n <= 3) return n;
+        int prev = 2;
+        int curr = 3;
+        for(int i = 4; i < n; i++){
+            int temp = prev;
+            prev = curr;
+            curr += temp;
         }
-        return ans;
+        return prev + curr;
     }
 }
