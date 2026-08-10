5b5203f6934f3928b5c2990c087719086ac811d9
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-10 03:02:56 +0300

    0 ms (100%) 43 MB (93.01%) .cs

--- LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cs
+++ LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cs
@@ -1,11 +1,14 @@
 public class Solution {
     public int MinCostClimbingStairs(int[] cost) {
         int n = cost.Length;
-        int[] dp = new int[n+1];
-        dp[n] = 0;
-        dp[n-1] = cost[n-1];
-        for(int i = n-2; i >= 0; i--)  dp[i] = cost[i] + Math.Min(dp[i+1] , dp[i+2]);
-        return Math.Min(dp[0] , dp[1]);
-    }
+        int oneStep = cost[n - 1];  
+        int twoSteps = 0;           
+        for (int i = n - 2; i >= 0; i--) {
+            int current = cost[i] + Math.Min(oneStep, twoSteps);
+            twoSteps = oneStep;
+            oneStep = current;
+        }
 
+        return Math.Min(oneStep, twoSteps);
+    }
 }
