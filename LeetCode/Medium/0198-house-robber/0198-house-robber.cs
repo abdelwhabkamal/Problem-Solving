cb96463266a669cc1001101c2f08b516d894c369
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-13 14:35:06 +0300

    0 ms (100%) 40.7 MB (87.41%) .cs

--- LeetCode/Medium/0198-house-robber/0198-house-robber.cs
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.cs
@@ -1,10 +1,11 @@
 public class Solution {
     public int Rob(int[] nums) {
         int n = nums.Length;
-        if(n <= 2) nums.Max();
-        int[] rob = new int[n + 1]; 
+        if(n == 1) return nums[0];
+        int[] rob = new int[n]; 
         rob[n - 1] = nums [n - 1];
-        for(int i = n - 2; i >=0; i--){
+        rob[n - 2] = Math.Max(nums [n - 1] , nums[n - 2]);
+        for(int i = n - 3; i >=0; i--){
             rob[i] = Math.Max(nums[i] + rob[i + 2] , rob[i + 1]);
         }
         return rob[0];
