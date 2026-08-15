443484555cdd90c5dcb92ce4d58f274863b0654e
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-15 06:18:22 +0300

    2 ms (100%) 38.6 MB (85.64%) .cs

---
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int CountSubstrings(string s) {
+        int n = s.Length;
+        int res = 0;
+
+        for(int center = 0; center < 2*n - 1; center++){
+            int left = center / 2;
+            int right = left + center % 2;
+
+            while(left >= 0 && right < n && s[left] == s[right]){
+                res++;
+                left--;
+                right++;
+            }
+        }
+        return res;
+    }
+}
