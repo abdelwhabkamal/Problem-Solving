cc14acd63ae1c16086f6bb7887fafdbe9c09b109
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-18 06:37:52 +0300

    4 ms (73.33%) 44.4 MB (60%) .cs

---
+++ LeetCode/Easy/3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int LargestInteger(int[] nums, int k) {
+        int n = nums.Length;
+        if(n == k) return nums.Max();
+        if(k == 1){
+           int[] count= new int[51];
+           for(int i = 0; i < n; i++) count[nums[i]]++;
+           int max = -1;
+           for(int i = 0; i < 51; i++){
+                if(count[i] == 1) max = Math.Max(max , i);
+            }
+            return max;
+        }
+        int f = nums[0]; 
+        int l = nums[n-1];
+        if(f == l) return -1;
+        for(int i = 1; i < n - 1; i++){
+            if(nums[i] == f) f = -1;
+            else if(nums[i] == l) l = -1;
+        }
+        return Math.Max(f,l);
+    }
+}
