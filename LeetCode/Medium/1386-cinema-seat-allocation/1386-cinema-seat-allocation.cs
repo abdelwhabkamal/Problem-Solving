47b31b16678f19d658c24344a1ec233f86c2e991
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-19 09:11:59 +0300

    32 ms (45%) 72.7 MB (5%) .cs

--- LeetCode/Medium/1386-cinema-seat-allocation/1386-cinema-seat-allocation.cs
+++ LeetCode/Medium/1386-cinema-seat-allocation/1386-cinema-seat-allocation.cs
@@ -1,21 +1,20 @@
 public class Solution {
     public int MaxNumberOfFamilies(int n, int[][] reservedSeats) {
-        Dictionary<int , HashSet<int>> seats = new();
-        for(int i = 0; i < reservedSeats.Length; i++){
-            if(!seats.ContainsKey(reservedSeats[i][0])) seats[reservedSeats[i][0]] = new HashSet<int>();
-            seats[reservedSeats[i][0]].Add(reservedSeats[i][1]);
+        Dictionary<int, HashSet<int>> seats = new();
+        foreach (var r in reservedSeats) {
+            if (!seats.ContainsKey(r[0])) seats[r[0]] = new HashSet<int>();
+            seats[r[0]].Add(r[1]);
         }
-        int groups = n*2;
-        foreach(var kvp in seats){
-            if(kvp.Value.Contains(4) || kvp.Value.Contains(5)) {
-                groups--;
-                if(kvp.Value.Contains(6) || kvp.Value.Contains(7) || kvp.Value.Contains(8) || kvp.Value.Contains(9)) groups--;
-            }
-            else if(kvp.Value.Contains(6) || kvp.Value.Contains(7)){
-                groups--;
-                if(kvp.Value.Contains(2) || kvp.Value.Contains(3) || kvp.Value.Contains(4) || kvp.Value.Contains(5)) groups--;
-            }
-            else if(kvp.Value.Contains(2) || kvp.Value.Contains(3) || kvp.Value.Contains(8) || kvp.Value.Contains(9)) groups--;
+
+        int groups = n * 2;
+        foreach (var kvp in seats) {
+            bool left = !(kvp.Value.Contains(2) || kvp.Value.Contains(3) || kvp.Value.Contains(4) || kvp.Value.Contains(5));
+            bool right = !(kvp.Value.Contains(6) || kvp.Value.Contains(7) || kvp.Value.Contains(8) || kvp.Value.Contains(9));
+            bool middle = !(kvp.Value.Contains(4) || kvp.Value.Contains(5) || kvp.Value.Contains(6) || kvp.Value.Contains(7));
+
+            if (left && right) continue;
+            else if (left || right || middle) groups--; 
+            else groups -= 2; 
         }
         return groups;
     }
