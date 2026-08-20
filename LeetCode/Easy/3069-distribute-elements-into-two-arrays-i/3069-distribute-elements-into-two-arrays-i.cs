8f1850de0042ccfa1a1df5d7427600fca027a3f0
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-20 10:59:45 +0300

    0 ms (100%) 49.2 MB (40%) .cs

--- LeetCode/Easy/3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cs
+++ LeetCode/Easy/3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cs
@@ -1,15 +1,12 @@
 public class Solution {
     public int[] ResultArray(int[] nums) {
-        List<int> arr2 = new();
-        arr2.Add(nums[1]);
+        int[] arr2 = new int[nums.Length - 1];
+        arr2[0] = nums[1];
         int l = 0;
         int r = 0;
         for(int i = 2; i < nums.Length; i++){
             if(nums[l] > arr2[r]) nums[++l] = nums[i];
-            else {
-                arr2.Add(nums[i]);
-                r++;
-            }
+            else arr2[++r] = nums[i];
         }
         for(int i = 0; i <= r; i++){
             nums[l+i+1] = arr2[i];
