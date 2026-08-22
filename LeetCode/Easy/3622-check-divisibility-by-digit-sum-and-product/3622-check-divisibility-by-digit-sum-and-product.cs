bac5673b3eda0617016a7249dc89599cc64bff79
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-22 11:01:15 +0300

    0 ms (100%) 30.5 MB (91.67%) .cs

---
+++ LeetCode/Easy/3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool CheckDivisibility(int n) {
+        int sum = 0;
+        int prod = 1;
+        int temp = n;
+        while(temp > 0 ){
+            sum += temp%10;
+            prod *= temp%10;
+            temp /=10;
+        }
+        return n % (sum + prod) == 0;
+    }
+}
