3e24e02b86eceb9fe276a7b199e3d89edc5b07b4
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-25 15:05:11 +0300

    4 ms (74.19%) 43.7 MB (87.1%) .cs

--- LeetCode/Easy/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cs
+++ LeetCode/Easy/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cs
@@ -1,11 +1,9 @@
 public class Solution {
     public int MissingMultiple(int[] nums, int k) {
-        int[] count = new int[101];
-        foreach(int num in nums)count[num]++;
+        HashSet<int> set =  new(nums);
         int res = k;
-        while(count[res] > 0){
+        while(set.Contains(res)){
             res += k;
-            if(res > 100) break;
         }
         return res;
     }
