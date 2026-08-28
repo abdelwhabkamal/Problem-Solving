958193cd5ac508b48a2a7b4d7fc6f7ad003980e2
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-28 15:52:49 +0300

    13 ms (99.22%) 46.3 MB (31.68%) .cs

--- LeetCode/Medium/0322-coin-change/0322-coin-change.cs
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public int CoinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        Array.Fill(dp , amount + 1);
-        dp[0] = 0;
+        for(int i = 1; i <= amount; i++) dp[i] = amount + 1;
         for(int i = 1; i <= amount; i++){
-            foreach(int coin in coins){
-                if(i >= coin){
-                    dp[i] = Math.Min(dp[i] , dp[i-coin] + 1);
+            foreach(int coin in coins) {
+                if(i - coin >= 0){
+                    dp[i] = Math.Min(dp[i] , dp[i - coin] + 1);
                 }
             }
         }
