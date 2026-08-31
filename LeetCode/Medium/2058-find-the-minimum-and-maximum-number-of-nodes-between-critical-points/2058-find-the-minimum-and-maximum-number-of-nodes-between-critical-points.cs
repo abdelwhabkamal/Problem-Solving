0ddea72e32876990b14e1d1d32fdd3b7244e3735
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-08-31 14:05:57 +0300

    3 ms (100%) 89.8 MB (77.78%) .cs

--- LeetCode/Medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cs
+++ LeetCode/Medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cs
@@ -13,23 +13,25 @@ public class Solution {
     public int[] NodesBetweenCriticalPoints(ListNode head) {
         int[] res = [-1,-1];
         int indx = 1;
-        List<int> points = new();
+        int prev = 0;
+        int first = 0;
+        int min = int.MaxValue;
         while(head.next.next !=  null){
-            if(head.next.val > head.val && head.next.val > head.next.next.val) points.Add(indx);
-            else if(head.next.val < head.val && head.next.val < head.next.next.val) points.Add(indx);
+            if((head.next.val > head.val && head.next.val > head.next.next.val)
+            || (head.next.val < head.val && head.next.val < head.next.next.val)){
+                if(first == 0){
+                    first = indx;
+                    prev = indx;
+                }
+                else{
+                    min = Math.Min(min, indx - prev);
+                    prev = indx; 
+                }
+            }
             head = head.next;
             indx++;
         }
-        int p = points.Count;
-        if(p > 1){
-            points.Sort();
-            int min = int.MaxValue;
-            for(int i = 0; i < p-1; i++){
-                min = Math.Min(min, points[i+1] - points[i]);
-            }
-            res[0] = min;
-            res[1] = points[p-1] - points[0];
-        }
+        if(first != prev) res = [min , prev - first];
         return res;
     }
 }
