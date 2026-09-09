321918b66b31f33cddee678141643a0e540fc119
Abdelwahab Kamal <97142176+abdelwhabkamal@users.noreply.github.com>
2026-09-09 05:22:53 +0300

    0 ms (100%) 29 MB (66.67%) .cs

---
+++ LeetCode/Medium/3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public long CountCommas(long n) {
+        if(n < 1000) return 0;
+        if(n < 1000000) return n - 999;
+        if(n < 1000000000) return 999000 + (n - 999999) * 2;
+        if(n < 1000000000000) return  1998999000 + (n - 999999999) * 3;
+        if(n == 1000000000000000) return 3998998998999005;
+        else return 2998998999000 +(n - 999999999999) * 4;
+    }
+}
